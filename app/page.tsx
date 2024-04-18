7513c506b006b449fa7aa0116a5cac593a31c813
kixixixixi <kixixixixi@gmail.com>
2024-04-19 02:13:11 +0900

    Fix position

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,19 @@ const Index: NextPage = () => {
     },
   })
   useEffect(() => {
+    const handleResize = () => {
+      const { innerHeight, innerWidth } = window
+      const w = Math.ceil(innerWidth / 256)
+      const h = Math.ceil(innerHeight / 256)
+      setTile({
+        width: w % 2 == 0 ? w + 1 : w,
+        height: h % 2 == 0 ? h + 1 : h,
+        offset: {
+          x: (innerWidth % 256) - (w % 2 == 0 ? 256 : 0),
+          y: (innerHeight % 256) - (h % 2 == 0 ? 256 : 0),
+        },
+      })
+    }
     window.addEventListener("resize", handleResize)
     navigator.geolocation.getCurrentPosition((position) => {
       setPosition(position)
@@ -28,69 +41,54 @@ const Index: NextPage = () => {
       window.removeEventListener("resize", handleResize)
     }
   }, [])
-  const handleResize = () => {
-    const { innerHeight, innerWidth } = window
-    setTile({
-      width: Math.ceil(innerWidth / 256),
-      height: Math.ceil(innerHeight / 256),
-      offset: {
-        x: (innerWidth % 256) / 4,
-        y: (innerHeight % 256) / 4,
-      },
-    })
-    console.log(innerWidth, Math.ceil(innerWidth / 256), (innerWidth % 256) / 2)
-  }
+
   return (
     <>
       <main>
-        <div
+        <section
           style={{
+            display: "grid",
+            height: `${tile.height * 256}px`,
+            overflow: "hidden",
             position: "fixed",
-            left: -tile.offset.x,
-            top: -tile.offset.y,
+            left: `${tile.offset.x / 2 - 128}px`,
+            top: `${tile.offset.y / 2}px`,
+            width: `${tile.width * 256}px`,
           }}
         >
-          <section
-            style={{
-              display: "grid",
-              overflow: "hidden",
-              position: "fixed",
-            }}
-          >
-            {position &&
-              fromGeoPositionToImageUrlList({
-                position,
-                z: 18,
-                ...tile,
-              }).map((row, i) =>
-                row.map((url, j) => (
-                  <div
-                    key={`${i}-${j}`}
-                    style={{
-                      backgroundImage: `url(${url})`,
-                      gridRowStart: j + 1,
-                      gridColumnStart: i + 1,
-                      height: 256,
-                      width: 256,
-                    }}
-                  />
-                ))
-              )}
-            <div
-              style={{
-                backgroundColor: "blue",
-                border: "solid 1px white",
-                borderRadius: "8px",
-                height: "12px",
-                left: "50dvw",
-                position: "fixed",
-                top: "50dvh",
-                width: "12px",
-                zIndex: 2,
-              }}
-            />
-          </section>
-        </div>
+          {position &&
+            fromGeoPositionToImageUrlList({
+              position,
+              z: 18,
+              ...tile,
+            }).map((row, i) =>
+              row.map((url, j) => (
+                <div
+                  key={`${i}-${j}`}
+                  style={{
+                    backgroundImage: `url(${url})`,
+                    gridRowStart: j + 1,
+                    gridColumnStart: i + 1,
+                    height: 256,
+                    width: 256,
+                  }}
+                />
+              ))
+            )}
+        </section>
+        <div
+          style={{
+            backgroundColor: "blue",
+            border: "solid 1px white",
+            borderRadius: "8px",
+            height: "12px",
+            left: "50vw",
+            position: "fixed",
+            top: "50dvh",
+            width: "12px",
+            zIndex: 2,
+          }}
+        />
         <section
           style={{
             position: "fixed",
